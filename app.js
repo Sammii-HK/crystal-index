8adbfb5957d6d1fa3a4031e5e708492aec28bf2b
Samantha Kellow <sammii.h@icloud.com>
2021-02-13 18:53:48 +0000

    resolving post to use .env variable

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
-// const port = process.env.PORT;
-const port = 3000
+const port = process.env.PORT;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
