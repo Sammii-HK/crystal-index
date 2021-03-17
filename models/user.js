26ae20015fe55427f45575d8761fef78330ba7a8
Samantha Kellow <sammii.h@icloud.com>
2021-03-17 21:07:21 +0000

    adjusting user model

--- models/user.js
+++ models/user.js
@@ -1,45 +1,42 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      User.Crystals.belongsToMany(Crystals, {
-        through: 'favourites',
-        as: 'crystals',
-        foreignKey: 'crystalId',
-      })
-    }
-  };
-  User.init({
+// const { Model } = require('sequelize');
+export default (sequelize, DataTypes) => {
+  // const Crystals = sequelize.models.Crystal;
+
+  const User = sequelize.define('User', {
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
     createdAt: new Date(),
     updatedAt: new Date(),
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  })
+  
+  // class User extends Model {
+  //   /**
+  //    * Helper method for defining associations.
+  //    * This method is not a part of Sequelize lifecycle.
+  //    * The `models/index` file will call this method automatically.
+  //    */
+  //   static associate(models) {
+  //     // define association here
+  //   }
+  // };
+  // User.init({
+  // }, {
+  //   sequelize,
+  //   modelName: 'User',
+  // });
+
+  User.associate = function(models) {
+    User.belongsToMany(models.Crystal, {
+      through: 'Favourites',
+      as: 'crystals',
+      foreignKey: 'userId',
+    });
+  };
+
   return User;
 };
 
-const usersWithCrystals = await User.findAll({
-  include: [{model: Crystal}]
-})
-
-console.log(usersWithCrystals[0])
-
-// const pugsWithFriends = await Pug.findAll({
-//   include: [{model: Friend}]
-// })
-
-// console.log(pugsWithFriends[0])
+console.log('User', User);
