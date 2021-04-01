4c6488bb526f578d0cb27f0a134f7cc6d747f433
Samantha Kellow <sammii.h@icloud.com>
2021-04-01 21:02:33 +0100

    deleting unnecessary route imports

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,11 +1,8 @@
 const ping = require('./ping');
 const users = require('./users');
-const crystals = require('./crystals');
-const favourites = require('./favourites');
+
 
 module.exports = [
   ...ping,
   ...users,
-  ...crystals,
-  ...favourites,
 ];
