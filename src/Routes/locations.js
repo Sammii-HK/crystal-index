5f589115dfbd1b3977a28d921bd6415651bbef0e
Samantha Kellow <sammii.h@icloud.com>
2021-04-22 09:43:43 +0100

    locations routes

---
+++ src/Routes/locations.js
@@ -0,0 +1,128 @@
+const db = require('../../models');
+
+module.exports = [{
+  method: 'POST',
+  path: '/locations',
+  handler: async (req, h) => {
+    const { 
+      placeName, 
+      country, 
+      lat, 
+      long, 
+    } = req.payload;
+    try {
+      const results = await db.location.create({
+        placeName, 
+        country,
+        lat,
+        long, 
+      });
+      return {
+        success: true,
+        id: results.id,
+      };
+    } catch (e) {
+      console.log('error creating location:', e);
+      return h.response(`Failed: ${e.message}`).code(500);
+    }
+  },
+}, {
+  method: 'GET',
+  path: '/locations',
+  handler: async (_, h) => {
+    try {
+      const results = await db.location.findAll({
+        // attributes: ['id', 'name'],
+      });
+      return results;
+    } catch (e) {
+      console.log('error creating location:', e);
+      return h.response(`Failed: ${e.message}`).code(500);
+    }
+  },
+}, {
+  method: 'GET',
+  path: '/locations/{id}',
+  handler: async (req, h) => {
+    const { id } = req.params;
+    try {
+      const results = await db.location.findAll({
+        where: { id: id },
+      });
+      return results;
+    } catch (e) {
+      console.log('error creating location:', e);
+      return h.response(`Failed: ${e.message}`).code(500);
+    }
+  },
+}, {
+  method: 'PUT',
+  path: '/locations/{id}',
+  handler: async (req, h) => {
+    const { id } = req.params;
+    const { 
+      placeName, 
+      country, 
+      lat,
+      long, 
+    } = req.payload;
+    const results = await db.location.findAll({
+      where: { id },
+    });
+    try {
+      const updatePromises = [];
+      const updateLocationsObject = await db.location.update({
+        placeName, 
+        country,
+        lat,
+        long,
+      }, {
+        where: { id },
+      });
+      updatePromises.push(updateLocationsObject); 
+
+      await Promise.all(updatePromises);
+
+      return results
+    } catch (e) {
+      console.log('error modifying location:', e);
+      return h.response(`Failed: ${e.message}`).code(500);
+    }
+  },
+}, {
+  method: 'DELETE',
+  path: '/locations/{crystalId}',
+  handler: async (req, h) => {
+    const { crystalId } = req.params;
+    try {
+      const results = await db.location.destroy({
+        where: { id: crystalId },
+      });
+      return {
+        success: true,
+        results,
+      };
+    } catch (e) {
+      console.log('error modifying location:', e);
+      return h.response(`Failed: ${e.message}`).code(500);
+    }
+  },
+}, {
+  method: 'DELETE',
+  path: '/locations/{crystalId}/{userId}',
+  handler: async (req, h) => {
+    const { crystalId, userId } = req.params;
+    try {
+      const results = await db.users_crystal.destroy({
+        where: { crystalId, userId },
+      });
+      return {
+        success: true,
+        results,
+      };
+    } catch (e) {
+      console.log('error modifying location:', e);
+      return h.response(`Failed: ${e.message}`).code(500);
+    }
+  },
+}];
