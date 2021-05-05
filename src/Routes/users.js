f6d177da2e319805d1be4d05c3fa1e5dc5cf5f3a
Samantha Kellow <sammii.h@icloud.com>
2021-05-05 19:42:31 +0100

    updating `user controller` to include `db.crystal` as 'favouriteCrystals` through `db.favourite` improved whitespace

--- src/Routes/users.js
+++ src/Routes/users.js
@@ -1,6 +1,7 @@
 const db = require('../../models');
 
-module.exports = [{
+module.exports = [
+  {
   method: 'POST',
   path: '/users',
   handler: async (req, h) => {
@@ -20,149 +21,168 @@ module.exports = [{
         }
       }, {
         include: [
-        {
-          model: db.userDetail,
-          as: 'userDetail',
-        }
-      ],
-
+          {
+            model: db.userDetail,
+            as: 'userDetail',
+          },
+        ],
       });
-      return {
-        success: true,
-        id: results.id,
+
+        return {
+          success: true,
+          id: results.id,
+        };
+      } catch (e) {
+        console.log('error creating user:', e);
+        return h.response(`Failed: ${e.message}`).code(500);
+      }
+    },
+  }, 
+  {
+    method: 'GET',
+    path: '/users',
+    handler: async (_, h) => {
+      try {
+        const results = await db.user.findAll({
+          // attributes: ['id', 'userName'],
+          // attributes: { exclude: ['password',] },
+          include: [
+            {
+              model: db.userDetail,
+              as: 'userDetail',
+              attributes: ['firstName'],
+            }, 
+            {
+              model: db.crystal,
+              as: 'createdCrystals',
+              attributes: ['name'],
+              
+            },
+            {
+              model: db.crystal,
+              as: 'favouriteCrystals',
+              attributes: ['name'],
+              through: {
+                model: db.favourite,
+              },
+            },
+          ],
+        });
+        return results;
+      } catch (e) {
+        console.log('error fetching users:', e);
+        return h.response(`Failed: ${e.message}`).code(500);
+      }
+    },
+  }, 
+  {
+    method: 'GET',
+    path: '/users/{id}',
+    handler: async (req, h) => {
+      try {
+        const { id } = req.params;
+        const results = await db.user.findAll({
+          where: { id },
+          include: [
+            {
+              model: db.userDetail,
+              as: 'userDetail',
+              attributes: ['firstName'],
+            }, {
+              model: db.crystal,
+              as: 'createdCrystals',
+            },
+            {
+              model: db.crystal,
+              as: 'favouriteCrystals',
+              through: {
+                model: db.favourite,
+              },
+            },
+          ],
+        });
+        return results;
+      } catch (e) {
+        console.log('error finding user:', e);
+        return h.response(`Failed: ${e.message}`).code(500);
+      }
+    },
+  }, 
+  {
+    method: 'PUT',
+    path: '/users/{id}',
+    handler: async (req, h) => {
+      const { id } = req.params;
+      const {
+        userName, password, firstName, lastName, mobileNum, address,
+      } = req.payload;
+
+      const updateUsersObject = {
+        userName,
       };
-    } catch (e) {
-      console.log('error creating user:', e);
-      return h.response(`Failed: ${e.message}`).code(500);
-    }
-  },
-}, {
-  method: 'GET',
-  path: '/users',
-  handler: async (_, h) => {
-    try {
-      const results = await db.user.findAll({
-        // attributes: ['id', 'userName'],
-        // attributes: { exclude: ['password',] },
-        include: [
-          {
-            model: db.userDetail,
-            as: 'userDetail',
-          }, {
-            model: db.crystal,
-            as: 'createdCrystals',
+
+      const updateUserDetailsObject = {
+        password, 
+        firstName, 
+        lastName,
+        mobileNum,
+        address,
+      };
+
+      try {
+        const updatePromises = [];
+        const updateUsersPromise = db.user.update(
+          updateUsersObject,
+          { where: { id } },
+        );
+        updatePromises.push(updateUsersPromise);
+
+        const updateUserDetailsPromise = db.userDetail.update(
+          updateUserDetailsObject,
+          { where: { id } },
+        );
+        updatePromises.push(updateUserDetailsPromise); 
+
+        await Promise.all(updatePromises);
+
+
+        const results = await db.user.findAll({
+          where: { id },
+          include: [
+            {
+              model: db.userDetail,
+              as: 'userDetail',
+            },
+          ],
+        });
+
+        return results
+
+      } catch (e) {
+        console.log('error updating user:', e);
+        return h.response(`Failed: ${e.message}`).code(500);
+      }
+    },
+  }, 
+  {
+    method: 'DELETE',
+    path: '/users/{id}',
+    handler: async (req, h) => {
+      try {
+        const { id } = req.params;
+        const results = await db.user.destroy({
+          where: {
+            id: id,
           },
-        ],
-      });
-      return results;
-    } catch (e) {
-      console.log('error fetching users:', e);
-      return h.response(`Failed: ${e.message}`).code(500);
-    }
-  },
-}, {
-  method: 'GET',
-  path: '/users/{id}',
-  handler: async (req, h) => {
-    try {
-      const { id } = req.params;
-      const results = await db.user.findAll({
-        where: { id },
-        include: [
-          {
-            model: db.userDetail,
-            as: 'userDetail',
-          }, {
-            model: db.crystal,
-            as: 'createdCrystals',
-          },
-        ],
-      });
-      return results;
-    } catch (e) {
-      console.log('error finding user:', e);
-      return h.response(`Failed: ${e.message}`).code(500);
-    }
-  },
-}, {
-  method: 'PUT',
-  path: '/users/{id}',
-  handler: async (req, h) => {
-    const { id } = req.params;
-    const {
-      userName, password, firstName, lastName, mobileNum, address,
-    } = req.payload;
-    const updateUsersObject = {
-      userName,
-    };
-    const updateUsersDetailsObject = {
-      password, 
-      firstName, 
-      lastName,
-      mobileNum,
-      address,
-    };
-    const results = await db.user.findAll({
-      where: { id },
-      include: [{
-        model: db.userDetail,
-        as: 'userDetail',
-      }],
-    });
+        });
+        return results;
+      } catch (e) {
+        console.log('error deleting user:', e);
+        return h.response(`Failed: ${e.message}`).code(500);
+      }
+    },
+  }
+];
 
-    try {
-      const updatePromises = [];
-      const updateUsersPromise = db.user.update(
-        updateUsersObject,
-        { where: { id } },
-      );
-      updatePromises.push(updateUsersPromise);
-
-      const updateUserDetailsPromise = db.userDetails.updae(
-        updateUsersDetailsObject,
-        { where: { id } },
-      );
-      updatePromises.push(updateUserDetailsPromise); 
-
-      await Promise.all(updatePromises);
-      // return 'users records updated';
-      return results
-    } catch (e) {
-      console.log('error updating user:', e);
-      return h.response(`Failed: ${e.message}`).code(500);
-    }
-  },
-}, {
-  method: 'DELETE',
-  path: '/users/{id}',
-  handler: async (req, h) => {
-    try {
-      const { id } = req.params;
-      const results = await db.user.destroy({
-        where: {
-          id: id,
-        },
-      });
-      return results;
-    } catch (e) {
-      console.log('error deleting user:', e);
-      return h.response(`Failed: ${e.message}`).code(500);
-    }
-  },
-}];
-
-// const updatePostsPromises = posts.map((p) => {
-      //   const { postTitle, postId } = p;
-      //   const updateObject = {
-      //     title: postTitle,
-      //   };
-      //   const whereQuery = {
-      //     id,
-      //     id: postId,
-      //   };
-      //   return db.posts.update(
-      //     updateObject,
-      //     { where: whereQuery },
-      //   );
-      // });
-      // updatePromises.push(...updatePostsPromises);
+// TODO: get `UPDATE` `.userDetails` working
+// TODO: work on `DELETE` on how to delete corresponding `.userDetails`
