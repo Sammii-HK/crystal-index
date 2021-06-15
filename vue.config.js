b382d970e896fcc4c073dc87640c85e1eb06f728
Samantha Kellow <sammii.h@icloud.com>
2021-06-15 18:13:15 +0100

    creating vue.config.js simplified V

--- vue.config.js
+++ vue.config.js
@@ -1,39 +1,26 @@
-const appConfig = require('./src/app.config')
+'use strict';
 
-/** @type import('@vue/cli-service').ProjectOptions */
 module.exports = {
-  // https://github.com/neutrinojs/webpack-chain/tree/v4#getting-started
-  chainWebpack(config) {
-    // We provide the app's title in Webpack's name field, so that
-    // it can be accessed in index.html to inject the correct title.
-    config.set('name', appConfig.title)
-
-    // Set up all the aliases we use in our app.
-    config.resolve.alias.clear().merge(require('./aliases.config').webpack)
-
-    // Don't allow importing .vue files without the extension, as
-    // it's necessary for some Vetur autocompletions.
-    config.resolve.extensions.delete('.vue')
-
-    // Only enable performance hints for production builds,
-    // outside of tests.
-    config.performance.hints(
-      process.env.NODE_ENV === 'production' &&
-        !process.env.VUE_APP_TEST &&
-        'warning'
-    )
-  },
-  css: {
-    // Enable CSS source maps.
-    sourceMap: true,
-  },
-  // Configure Webpack's dev server.
-  // https://cli.vuejs.org/guide/cli-service.html
+  publicPath: process.env.NODE_ENV === 'development'
+    ? '/crystal-app/'
+    : '/',
+  lintOnSave: false,
   devServer: {
-    ...(process.env.API_BASE_URL
-      ? // Proxy API endpoints to the production base URL.
-        { proxy: { '/api': { target: process.env.API_BASE_URL } } }
-      : // Proxy API endpoints a local mock API.
-        { before: require('./tests/mock-api') }),
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        ws: true,
+        changeOrigin: true,
+      },
+    },
   },
-}
+  configureWebpack: {
+    optimization: {
+      splitChunks: {
+        minSize: 10000,
+        maxSize: 250000,
+      },
+    },
+  },
+  chainWebpack: config => config.plugins.delete('prefetch'),
+};
