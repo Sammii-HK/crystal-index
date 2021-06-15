b35a9d586ea0952da2540a7a7daa26c863bd35a1
Samantha Kellow <sammii.h@icloud.com>
2021-06-15 19:15:07 +0100

    adding proxy to devServer

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,18 @@
 module.exports = {
+  lintOnSave: process.env.NODE_ENV !== 'production',
   devServer: {
     overlay: {
       warnings: true,
       errors: true
+    },
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000/',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
     }
-  }
+  },
 }
