c184171cc80032fc67a8e81ad4f771b151f5228a
Andrew Beveridge <andrew.beveridge@transferwise.com>
2021-06-23 19:30:12 +0100

    Added working docker-compose file to provide example of how this could work - but DB still doesn't have correct schema, not sure how sequelize expects to be run

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ module.exports = {
     },
     proxy: {
       '/api': {
-        target: 'http://localhost:3000/',
+        target: 'http://crystalsbackend:3000/',
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
