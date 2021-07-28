ad42103293224747f9332a3f7cc0d357c6955c99
Samantha Kellow <sammii.h@icloud.com>
2021-07-29 00:13:40 +0100

    updating navbar routes

--- src/components/CNavBar.vue
+++ src/components/CNavBar.vue
@@ -2,18 +2,15 @@
   <b-navbar>
     <template #brand>
       <b-navbar-item tag="router-link" :to="{ path: '/' }">
-        <img
-          src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
-          alt="Lightweight UI components for Vue.js based on Bulma"
-        >
+        <p class="subtitle">The Crystal Index</p>
       </b-navbar-item>
     </template>
     <template #start>
-      <b-navbar-item href="#">
-        Home
+      <b-navbar-item href="/">
+        About
       </b-navbar-item>
-      <b-navbar-item tag="router-link" :to="{ path: 'crystals-gallery' }">
-        Crystals Gallery
+      <b-navbar-item href="/crystal-form">
+        Create Crystal
       </b-navbar-item>
     </template>
 
