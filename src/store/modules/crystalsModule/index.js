c9d662aebb73f9fff1792ce4660e8fc36b58be4d
Samantha Kellow <sammii.h@icloud.com>
2021-07-29 00:15:55 +0100

    added single search to crystals store module

--- src/store/modules/crystalsModule/index.js
+++ src/store/modules/crystalsModule/index.js
@@ -1,32 +1,48 @@
 import axios from 'axios';
 
-const state = {
-  crystals: [] 
-}
-
-const mutations = {
-  UPDATE_CRYSTALS (state, payload) {
-    state.crystals = payload;
+const defaultState = () => {
+  return {
+    crystals: [],
+    crystal: {
+      id: null,
+    },
   }
 }
 
-const actions = {
-  getCrystals ({ commit }) {
-    axios.get('/api/crystals').then((response) => {
-      commit('UPDATE_CRYSTALS', response.data)
-    });
-  }
+export default {
+  state: defaultState(),
+  mutations: {
+    UPDATE_CRYSTALS (state, payload) {
+      state.crystals = payload;
+    },
+    UPDATE_CRYSTAL (state, payload) {
+      state.crystal = payload
+    },
+    // USER_UPDATE(state, payload) {
+    //   state[payload.key] = payload.value
+    // },
+  },  
+  actions: {
+    updateCrystal({ commit }, updatedValues) {
+      commit('UPDATE_CRYSTAL', updatedValues)
+    },
+    getCrystal ({ commit }, id) {
+      axios.get(`/api/crystals/${id}`).then((response) => {
+        const crystal = response.data[0]
+        
+        console.log("getCrystal", crystal);
+        commit('UPDATE_CRYSTAL', crystal)
+      });
+    },
+    getCrystals ({ commit }) {
+      axios.get('/api/crystals').then((response) => {
+        commit('UPDATE_CRYSTALS', response.data)
+      });
+    },
+  },
+  getters: {
+    crystals: state => state.crystals,
+    crystal: state => state.crystal,
+  },
 }
 
-const getters = {
-  crystals: state => state.crystals
-}
-
-const crystalsModule = {
-  state,
-  mutations,
-  actions,
-  getters
-}
-
-export default crystalsModule;
