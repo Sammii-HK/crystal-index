9ccdabfc87154904c018fb662b1b009ebee9b9fd
Samantha Kellow <sammii.h@icloud.com>
2021-07-29 00:15:04 +0100

    creating CrystalsGallery with router push to CrystalView

--- src/views/CrystalsGallery.vue
+++ src/views/CrystalsGallery.vue
@@ -1,10 +1,41 @@
 /* eslint-disable */
 
 <template>
-  <div class="container">
-    <div class="section">
-      <div v-for="crystal in crystals" :key="crystal.id">
+  <div class="container pt-4">
+    <div class="columns is-multiline is-mobile">
+      <div class="column is-12">
+        <div class="columns is-centered">
+          <div class="column is-3">
+            <b-field>
+              <b-input placeholder="Search..."
+              type="search"
+              icon="magnify"
+              icon-clickable
+              >
+              <!-- @icon-click="searchIconClick" -->
+              </b-input>
+            </b-field>
+
+            <div class="column is-3">
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <div
+      v-for="crystal in crystals" 
+      :key="crystal.id"
+      class="column is-4"
+      >
         {{ crystal.name }}
+        <figure class="image" @click="selectedCrystal(crystal.id)">
+          <b-image
+          :src="crystal.image" 
+          :alt="crystal.name"
+          ratio="1by1"
+          />
+        </figure>
       </div>
     </div>
   </div>
@@ -21,6 +52,11 @@ export default {
   },
   created() {
     this.$store.dispatch("getCrystals");
+  },
+  methods: {
+    selectedCrystal(id) {
+      this.$router.push(`/crystals/${id}`)
+    },
   },
 }
 </script>
