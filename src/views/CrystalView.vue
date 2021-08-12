8249fe560be644b0040c93157a0a3236ffb05e4c
Samantha Kellow <sammii.h@icloud.com>
2021-08-12 20:57:11 +0100

    created base crystal view

--- src/views/CrystalView.vue
+++ src/views/CrystalView.vue
@@ -1,25 +1,42 @@
 /* eslint-disable */
 
 <template>
-  <div class="container pt-4">
-    <p class="title mt-4 mb-6">{{ crystal.name }}</p>
-    <div class="columns is-multiline is-mobile is-centered">
-      <div class="column is-4" >
-        <figure class="image">
-          <b-image
-          :src="crystal.image" 
-          :alt="crystal.name"
-          ratio="1by1"
-          />
-        </figure>
-        <div class="container mt-6">
-          <p>{{crystal.colour}}</p>
-          <p>{{crystal.chakra}}</p>
+  <div class="section">
+    <div class="container pt-4">
+      <div class="columns is-multiline is-mobile is-centered">
+        <div class="column is-6" >
+          <figure class="image">
+            <b-image
+            :src="crystal.image" 
+            :alt="crystal.name"
+            ratio="1by1"
+            />
+          </figure>
+        </div>
+        <div class="column is-4 is-offset-1" >
+          <p class="title mt-0 mb-6">{{ crystal.name }}</p>
+          <div>
+            <b-taglist class="mt-3">
+              <b-tag v-for="colour in crystal.colour" :key="colour">
+                {{colour}}
+              </b-tag>
+            </b-taglist>
+
+            <b-taglist class="mt-4">
+              <b-tag v-for="chakra in crystal.chakra" :key="chakra">
+                {{chakra}}
+              </b-tag>
+            </b-taglist>
+
+          </div>
+          <div class="mt-4">
+            <p class="mt-6">{{crystal.bio}}</p>
+            <p class="mt-6">Origin: {{crystal.origin.placeName}}</p>
+            <p class="mt-6">Memento: {{crystal.memento.placeName}}</p>
+
+          </div>
 
         </div>
-      </div>
-      <div class="column is-4 is-offset-1" >
-        <p>{{crystal.bio}}</p>
       </div>
     </div>
   </div>
@@ -36,20 +53,20 @@ export default {
   },
   computed: {
     ...mapGetters([
-      "crystal"
+      "crystal",
     ])
   },
   mounted() {
     this.crystalId = this.$route.params.id
-    this.loadCrystal()
+    this.loadCrystal(this.crystalId)
   },
   methods: {
     ...mapActions({
       // get the action from the store
       getCrystal: 'getCrystal',
     }),
-    loadCrystal() {
-      this.$store.dispatch("getCrystal", this.crystalId);
+    async loadCrystal(id) {
+      await this.$store.dispatch("getCrystal", id);
     },
   },
   updated() {
