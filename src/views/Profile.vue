0155b49672fcc6fd87ccf47b4b46a03dfd16a574
Samantha Kellow <sammii.h@icloud.com>
2021-08-17 21:48:54 +0100

    base profile component

---
+++ src/views/Profile.vue
@@ -0,0 +1,48 @@
+/* eslint-disable */
+
+<template>
+  <div class="section">
+    <div class="container pt-4">
+      {{ authUser }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+export default {
+  name: "profile",
+  data() {
+    return {
+      userId: null,
+    }
+  },
+  computed: {
+    ...mapGetters([
+      "authUser",
+    ])
+  },
+  mounted() {
+    // this.userId = this.$route.params.id
+    // this.loadUser(this.userId)
+    console.log("this.authUser", this.authUser);
+    
+  },
+  methods: {
+    ...mapActions({
+      // get the action from the store
+      // getUser: 'getUser',
+    }),
+    // async loadUser(id) {
+    //   // await this.$store.dispatch("getUser", id);
+    // },
+  },
+  updated() {
+    
+  },
+}
+</script>
+
+<style>
+
+</style>
