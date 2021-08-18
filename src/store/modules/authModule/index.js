f28237c8989e1c05e4b993f62d1940720ba0f575
Samantha Kellow <sammii.h@icloud.com>
2021-08-18 19:19:34 +0100

    first attempt of auth_user store or validation

--- src/store/modules/authModule/index.js
+++ src/store/modules/authModule/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const defaultState = () => ({ 
   token: null,
-  user: {},
+  auth_user: {},
 })
 
 export default {
@@ -17,23 +17,34 @@ export default {
     CLEAR_TOKEN(state) {
       state = defaultState();
       console.log("state", state);
-      console.log("state.token", state.token);
+    },
+    AUTH_USER_UPDATE(state, payload) {
+      state.auth_user[payload.key] = payload.value
     },
   },
   actions: {
-    async setAuthenticatedUser({ commit, dispatch }, { user }) {     
+    async setAuthenticatedUser({ commit }, { user }) {     
       await axios.post('/api/login', user).then((response) => {
         console.log("response", response);
         commit('CLEAR');
         commit('SET_TOKEN', response.data.credentials);
-        dispatch('updateUser', user);
+        // dispatch('updateUser', user);
+        commit('AUTH_USER_UPDATE', user)
         console.log("login success");
       });
+    },
+    logOut({ commit }) {
+      // commit('CLEAR')
+      commit('CLEAR_TOKEN')
+      // commit('SET_TOKEN', null, { root: true })
+      console.log("logged out");
+      // router.push('/');
     },
   },
   getters: {
     // isAuthorised: (state) => !!state.token,
     isLoggedIn: (state) => state.token,
+    authUser: (state) => state.auth_user
     // isLoggedIn: (state) => state.response.success,
   },
 }
