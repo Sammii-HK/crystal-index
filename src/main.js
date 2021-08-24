f4334545f93125996f09882d849ab83c9b7b5dfb
Samantha Kellow <sammii.h@icloud.com>
2021-08-24 17:34:58 +0100

    importing main.scss to main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import Vue from "vue";
 import App from "./App.vue";
+
 import router from "./router";
 import store from "./store";
+
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 
+import "@/styles/main.scss";
+
 Vue.use(Buefy)
+
 
 Vue.config.productionTip = false;
 
