2bf632e1d267ec62fffe74fe5f033e685718fd00
Samantha Kellow <sammii.h@icloud.com>
2021-10-07 22:41:00 +0100

    amending gallery view back to crystal.name

--- src/views/CrystalsGallery.vue
+++ src/views/CrystalsGallery.vue
@@ -35,7 +35,7 @@
         @mouseleave="toggleOverlay(crystal.id, false)"
         >
           <b-image
-          :src="`http://192.168.1.34:94/crystals/${crystal.id}.png`" 
+          :src="crystal.image" 
           :alt="crystal.name"
           ratio="1by1"
           />
