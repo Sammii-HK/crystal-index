a378f47b1993568fda737baa2a26050d9fba1ca1
Samantha Kellow <sammii.h@icloud.com>
2021-10-15 00:21:04 +0100

    amending config files for dotenv

--- crystal-api/config/environment.js
+++
@@ -1,6 +0,0 @@
-const secret = process.env.SECRET || 'Zge{T*g._&;(gCaQ2mcn=-mR'
-// const dbUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/on-set-london'
-const port = process.env.PORT || 3000
-const tokenExpiry = 14400 // 4 hours
-
-module.exports = { secret, port, tokenExpiry }
