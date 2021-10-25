62016e525bd52aea7167a61b620cc64e0e3e4043
Samantha Kellow <sammii.h@icloud.com>
2021-10-25 19:55:40 +0100

    amending config files for dotenv updating DB vars to use process.env values + updating import path

--- crystal-api/config/auth_config.js
+++
@@ -1,6 +0,0 @@
-const issuerBaseURL = "sammii.eu.auth0.com"
-const clientId = "81j7QJ3yXEwgCPhNxLmlfFCQyl5CPcor"
-const baseURL = "http://localhost:3000"
-const secret = "8ac7f8682ff34304e28b1281d44cc577e4cb917bbc62dd1c6e3ef4f3b03a4cab"
-
-module.exports = { issuerBaseURL, clientId, baseURL, secret };
