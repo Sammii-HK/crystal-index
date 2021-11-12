4815a608fb5adc43a90e106e17232520f4f50a4b
Samantha Kellow <sammii.h@icloud.com>
2021-11-12 16:55:48 +0000

    registering verifcationBus as authUser basic strategy

--- crystal-api/server.js
+++ crystal-api/server.js
@@ -2,12 +2,16 @@
 
 const Hapi = require('@hapi/hapi');
 const Routes = require('./controller');
+const { verificationBus } = require('./lib/secureRoute.js')
 
 const init = async () => {
   const server = Hapi.Server({
     host: `0.0.0.0`,
     port: 3000,
   });
+  await server.register(require('@hapi/basic'));
+  server.auth.strategy('authUser', 'basic', { validate: verificationBus });
+
   server.route(Routes);
   await server.start();
   console.log('🦄 Server running at port:', server.info.uri);
