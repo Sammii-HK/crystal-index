ac53937fc0447e47dfb3ad2a387f62247c6cf4b9
Samantha Kellow <sammii.h@icloud.com>
2021-11-12 16:58:46 +0000

    adding to crystal controller

--- src/store/modules/crystalsModule/index.js
+++ src/store/modules/crystalsModule/index.js
@@ -14,9 +14,12 @@ export default {
     UPDATE_CRYSTAL (state, payload) {
       state.crystal = payload
     },
+    CLEAR_STATE(state) {
+      Object.assign(state, defaultState())      
+    },
   },  
   actions: {
-    updateCrystal({ commit }, updatedValues) {
+    updateCrystalState({ commit }, updatedValues) {
       commit('UPDATE_CRYSTAL', updatedValues)
     },
     getCrystal({ commit }, id) {
@@ -29,12 +32,24 @@ export default {
     getCrystals({ commit }) {
       axios.get('/api/crystals')
       .then((response) => {
+        commit('CLEAR_STATE');
         commit('UPDATE_CRYSTALS', response.data)
       });
     },
     createCrystal({ commit }, crystal) {
       axios.post('/api/crystals/create', crystal)
       .then((response) => {
+        commit('CLEAR_STATE');
+        commit('UPDATE_CRYSTAL', response.data)
+      });
+    },
+    updateCrystal({ commit }, crystal, token) {
+      let config = {
+        headers: { token }
+      }
+      axios.put(`/api/crystals/${crystal.id}`, crystal, config)
+      .then((response) => {
+        commit('CLEAR_STATE');
         commit('UPDATE_CRYSTAL', response.data)
       });
     },
