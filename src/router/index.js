b00b1b969748763298141491bf8d372eb5f70296
Samantha Kellow <sammii.h@icloud.com>
2021-11-12 17:32:43 +0000

    aqdding requiresAuth per route, if unauthed, puth to login

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import store from "../store";
 
 Vue.use(VueRouter);
 
@@ -25,26 +26,27 @@ const routes = [
       import(/* webpackChunkName: "about" */ "../views/About"),
   },
   {
-    path: "/crystals-gallery",
-    name: "Crystals Gallery",
-    component: () => import("../views/CrystalsGallery") 
-  },
-  {
     path: "/crystal-create",
     name: "Crystal Create Form",
-    component: () => import("../views/CrystalCreateForm") 
+    component: () => import("../views/CrystalCreateForm"),
+    meta: { requiresAuth: true },
   },
   {
     path: '/crystals/:id/',
     name: "View Crystal",
-    // component: () => import("../views/CrystalForm") 
     component: () => import("../views/CrystalView") 
+  },
+  {
+    path: '/crystals/:id/update',
+    name: "Update Crystal",
+    component: () => import("../views/CrystalUpdate"),
+    meta: { requiresAuth: true },
   },
   {
     path: '/profile',
     name: "Profile",
-    // component: () => import("../views/CrystalForm") 
-    component: () => import("../views/Profile") 
+    component: () => import("../views/Profile"),
+    meta: { requiresAuth: true },
   },
 ];
 
@@ -53,5 +55,15 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes,
 });
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!store.authUser?.id) {
+      next({
+        path: '/login',
+      })
+    } else next()
+  } else next() // make sure to always call next()!
+})
 
 export default router;
