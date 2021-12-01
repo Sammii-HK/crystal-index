6c6ff474709bcfa054ee1fb588ab66014635d4df
Samantha Kellow <sammii.h@icloud.com>
2021-12-01 19:14:42 +0000

    userModule getUser profile action

--- src/store/modules/userModule/index.js
+++ src/store/modules/userModule/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const defaultState = () => ({
   user: {},
 });
@@ -7,7 +9,11 @@ export default {
   namespaced: true,
   mutations: {
     UPDATE_USER(state, payload) {
-      state[payload.key] = payload.value
+      // if ([payload.key] !== 'password') state.user[payload.key] = payload.value
+      state.user[payload.key] = payload.value
+    },
+    SET_USER(state, payload) {
+      state.user = { ...state.user, ...payload };
     },
     CLEAR_USER(state) {
       Object.assign(state, defaultState())      
@@ -16,10 +22,21 @@ export default {
   actions: {
     updateUser({ commit }, updatedValues) {
       commit('CLEAR_USER')
+      // TODO: password on login saved to store state
       commit('UPDATE_USER', updatedValues)
     },
     clearUser({ commit }) {
       commit('CLEAR_USER')
+    },
+    getUser({ commit }, { id, token }) {
+      let config = {
+        headers: { Authorization: 'Bearer ' + token }
+      }      
+      axios.get(`/api/profile/${id}`, config)
+      .then((response) => {
+        const user = response.data[0]
+        commit('SET_USER', user)
+      });
     },
   },
   getters: {
