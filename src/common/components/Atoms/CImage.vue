49a860e3dc7396ac49ee6c244b50fe7654f99d83
Samantha Kellow <sammii.h@icloud.com>
2021-12-29 19:23:28 +0000

    CImage refactor, `:src="`/api/images/${imageId}`"`

--- src/common/components/Atoms/CImage.vue
+++ src/common/components/Atoms/CImage.vue
@@ -1,7 +1,7 @@
 <template>
   <b-image
-  :src="dataUrl"
-  :alt="image.name"
+  :src="`/api/images/${imageId}`"
+  :alt="`crystal image ${imageId}`"
   ratio="1by1"
   />
 </template>
@@ -9,35 +9,11 @@
 <script>
 export default {
   name: 'c-image',
-  data() {
-    return {
-      file: null,
-    }
-  },
   props: {
-    image: {
-      type: Object,
+    imageId: {
+      type: Number,
       required: true,
     }
   },
-  computed: {
-    dataUrl() {
-      const base64 = arrayBufferToBase64(new Uint8Array(this.image.file.data));
-      return `data:${this.image.type};base64,${base64}`
-    },
-  },
-}
-
-function arrayBufferToBase64(bytes) {
-    let base64 = '';
-    const len = bytes.byteLength;
-    for (var i = 0; i < len; i++) {
-        base64 += String.fromCharCode(bytes[i]);
-    }
-    return btoa(base64)
 }
 </script>
-
-<style>
-
-</style>
