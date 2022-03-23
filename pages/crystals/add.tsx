367b757aab9fccdcb7cb270d8cde8fb33c17f955
Samantha Kellow <sammii.h@icloud.com>
2022-03-23 17:43:18 +0000

    updating crystal add form to use setState

--- pages/crystals/add.tsx
+++ pages/crystals/add.tsx
@@ -1,10 +1,27 @@
 import axios from 'axios';
 import { Crystal } from '@prisma/client';
-import { FormEventHandler, useCallback } from 'react'
-import { BField, BInput, BTextArea, BSelect } from '../../components/Atoms';
-import { BTagContainer } from '../../components/Molecules';
+import { FormEventHandler, useCallback, useState } from 'react'
+import { BField, BInput, BTextArea } from '../../components/Atoms';
+import { BTags } from '../../components/Molecules';
+import useUserId from '../../lib/hooks';
 
-const crystalFields = [
+type CrystalState = {
+  name: string,
+  bio: string,
+  otherNames: string,
+  colour: string[],
+  chakra: string[]
+  createdById: string,
+}
+
+const crystalFields: {
+  key: keyof CrystalState,
+  component: React.FC<any>,
+  label: string,
+  placeHolder: string,
+  required: boolean,
+  options?: string[]
+}[] = [
   {
     key: 'name',
     component: BInput,
@@ -28,7 +45,7 @@ const crystalFields = [
   },
   {
     key: 'colour',
-    component: BTagContainer,
+    component: BTags,
     label: 'Colour',
     placeHolder: 'Select colour(s)',
     options: [ 'red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet', 'black', 'white', 'clear' ],
@@ -36,63 +53,66 @@ const crystalFields = [
   },
   {
     key: 'chakra',
-    component: BTagContainer,
+    component: BTags,
     label: 'Chakra',
     placeHolder: 'Select chakra(s)',
     options: [ 'crown', 'third eye', 'throat', 'heart', 'solar plexus', 'sacral', 'root' ],
     required: false,
   },
-]
+];
 
 const CreateCrystals: React.FC = () => {
+  const { userId } = useUserId();
+  // const session = await getSession({ req });
+  const [crystalState, setCrystalState] = useState<CrystalState>({
+    name: "",
+    bio: "",
+    otherNames: "",
+    chakra: [],
+    colour: [],
+    // createdById: userId
+  })
 
   const createCrystal: FormEventHandler = useCallback(async (event) => {
     event.preventDefault();
 
-    const target = event.target as typeof event.target & {
-      name: {value: string}
-      bio: {value: string}
-      otherNames: {value: string}
-      colour: {value: string[]}
-      chakra: {value: string[]}
-    };
-
-    const crystalData = {
-      name: target.name.value,
-      bio: target.bio.value,
-      otherNames: target.otherNames.value,
-      colour: target.colour.value,
-      chakra: target.chakra.value,
-    };
-
-
-    const res = await axios.post<{crystal?: Crystal, error: string}>('/api/crystal/create', 
-    crystalData,
-    { headers: { 'Content-Type': 'application/json' } }
+    const res = await axios.post<{crystal?: Crystal, error: string}>(
+      '/api/crystal/create', 
+      crystalState,
+      { headers: { 'Content-Type': 'application/json' } }
     );
   
     const result = await res.data;
     console.log("Crystal create API result", result);
-  }, []);
+  }, [crystalState]);
 
 
   return (
     <div className="section">
       <div>
-      <form onSubmit={createCrystal}>
-        {crystalFields.map(field => (
-          <BField label={field.label} key={field.key}>
-            <field.component 
-            id={field.key} 
-            placeholder={field.placeHolder} 
-            required={field.required}
-            tags={field.options}
-            />
-          </BField>
-        ))}
-        <button type="submit" className="button">Create</button> 
-      </form>
-    </div>
+        <form onSubmit={createCrystal}>
+          {crystalFields.map(field => (
+            <BField label={field.label} key={field.key}>
+              <field.component 
+                id={field.key} 
+                placeholder={field.placeHolder} 
+                required={field.required}
+                options={field.options}
+                value={crystalState[field.key]}
+                onChange={(newValue: any) => {
+                  setCrystalState((oldCrystalState) => ({...oldCrystalState, [field.key]: newValue}))
+                }}
+              />
+            </BField>
+          ))}
+          <button type="submit" className="button">Create</button> 
+        </form>
+        <hr />
+        {JSON.stringify(crystalState)}
+        <hr />
+        {JSON.stringify(userId)}
+
+      </div>
     </div>
   )
 }
