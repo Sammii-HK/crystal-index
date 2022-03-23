90b43e2d7c638d0feb26e0652d865232f35b31e1
Samantha Kellow <sammii.h@icloud.com>
2022-03-23 12:24:32 +0000

    exporting files in indexes and file tidy

--- components/Atoms/index.ts
+++ components/Atoms/index.ts
@@ -1,2 +1,6 @@
 export { default as BField } from './BField';
 export { default as BInput } from './BInput';
+export { default as BSelect } from './BSelect';
+export { default as BTextArea } from './BTextArea';
+export { default as BTag } from './BTag';
+export { default as BTags } from './BTags';
---
+++ components/Molecules/index.ts
@@ -0,0 +1,1 @@
+export { default as BTagContainer } from './BTagContainer';
