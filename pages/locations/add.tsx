65b4be48a066140619fe5fa641cdb3ec62bc3343
Samantha Kellow <sammii.h@icloud.com>
2023-01-09 22:25:12 +0000

    refactoring location/add to use NewLocationForm

--- pages/locations/add.tsx
+++ pages/locations/add.tsx
@@ -1,25 +1,10 @@
-import axios from 'axios';
-import { Location } from '@prisma/client';
-import { FormEventHandler, useCallback, useState } from 'react'
-import { LocationForm } from '../../components/Organisms';
+import { NewLocationForm } from '../../components/Organisms';
+import { createLocation } from '../../lib/helpers/locationRequests';
 
 const CreateLocations: React.FC = () => {
-
-  const createLocation = useCallback(async (location) => {
-    const res = await axios.post<{location?: Location, error: string}>(
-      '/api/location/create', 
-      location,
-      { headers: { 'Content-Type': 'application/json' } }
-    );
-  
-    const result = res.data;
-    console.log("Location create API result", result);
-  }, []);
-
-
   return (
     <div className="my-2">
-      <LocationForm onCreateLocation={createLocation} />
+      <NewLocationForm onCreateLocation={createLocation} />
     </div>
   )
 }
