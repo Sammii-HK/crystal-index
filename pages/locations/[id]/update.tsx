92bda0d2175415695e1df634023174b013fb64cf
Samantha Kellow <sammii.h@icloud.com>
2023-01-10 18:45:58 +0000

    WIP update location

--- pages/locations/[id]/update.tsx
+++ pages/locations/[id]/update.tsx
@@ -1,16 +1,19 @@
 import axios from 'axios';
-import { Location } from '@prisma/client';
 import { useCallback } from 'react'
 import useUser from '../../../lib/hooks';
 import type { RestrictedReactFC } from '../../../lib/hooks'
 import { useRouter } from 'next/router';
 import { GetServerSideProps } from 'next';
-import prisma from '../../../lib/prisma';
 import { LocationForm } from '../../../components/Organisms';
+import { findLocation } from '../../../lib/helpers/locationRequests';
+import { ViewLocationProps } from '../../../lib/types/location';
 
 const UpdateLocation: RestrictedReactFC<ViewLocationProps> = (props) => {
   const { role } = useUser()
   const router = useRouter()
+
+  console.log("props", props);
+  
 
   const updateLocation = useCallback(async (location) => {
     const res = await axios.put<{location?: Location, error: string}>(
@@ -23,12 +26,12 @@ const UpdateLocation: RestrictedReactFC<ViewLocationProps> = (props) => {
     console.log("Location update API result", result);
   }, []);
 
-  if (!props.location) return <p>No location found</p>
+  if (!props) return <p>No location found</p>
   return (
     <div className="section">
       <LocationForm 
         onCreateLocation={updateLocation}
-        location={props.location}
+        location={props}
       />
     </div>
   )
@@ -38,19 +41,12 @@ export default UpdateLocation
 
 UpdateLocation.requireAuth = true
 
-
-type ViewLocationProps = {
-  location: null | Location
-}
-
 export const getServerSideProps: GetServerSideProps<ViewLocationProps> = async (context) => {
   const { id } = context.params!;
-  const location = await prisma().location.findUnique({ where: { id: parseInt(id as string) } });
+  const location = await findLocation(id as string);
 
   console.log(`UPDATE Location ${id} result: `, location)
   return { 
-    props: {
-      location: location,
-    }
+    props: { ...location }
   }
 }
