b4ac8347750f4883baa1a74795434039ad3b28c4
Samantha Kellow <sammii.h@icloud.com>
2023-02-17 19:47:28 +0000

    add helprs CWithRelations + findAndSerializeCWLoc

--- lib/helpers/serializeCrystalDates.ts
+++ lib/helpers/serializeCrystalDates.ts
@@ -1,26 +1,58 @@
+import { Crystal, User, Image } from "@prisma/client";
 import prisma from "../prisma";
+
+type CrystalWithRelations = Crystal & {
+  createdBy: User,
+  image: Image[],
+  originLocation: { placeName: string } | null,
+  mementoLocation: { placeName: string } | null,
+  favouritedBy: { id: string; }[],
+}
+
+const serializeCrystal = (crystal: CrystalWithRelations) => ({
+  ...crystal,
+  createdAt: crystal.createdAt.toISOString(),
+  updatedAt: crystal.updatedAt.toISOString(),
+  image: crystal.image.map(image => image.id),
+  mementoLocation: crystal.mementoLocation?.placeName || null,
+  originLocation: crystal.originLocation?.placeName || null,
+  favouritedBy: crystal.favouritedBy.map(user => user.id)
+});
 
 export const findAndSerializeCrystal = async (id: string): Promise<any> => {
   const crystal = await prisma().crystal.findUnique(
     { 
-      where: { id: parseInt(id as string) },
+      where: { id: parseInt(id) },
       include: {
         createdBy: true,
         image: true,
+        originLocation: { select: { placeName: true }},
+        mementoLocation: { select: { placeName: true }},
+        favouritedBy: { select: { id: true }},
       },
     }
   );
-  const allLocations = await prisma().location.findMany();
 
-  const serialisableCrystal = crystal && {
-    ...crystal,
-    createdAt: crystal.createdAt.toISOString(),
-    updatedAt: crystal.updatedAt.toISOString(),
-    image: crystal.image.map(image => image.id),
-  };
-  
+  return crystal && serializeCrystal(crystal);
+};
+
+export const findAndSerializeCrystalWithLocations = async (id: string): Promise<any> => {
   return {
-    crystal: serialisableCrystal,
-    locations: allLocations,
+    crystal: await findAndSerializeCrystal(id),
+    locations: await prisma().location.findMany()
   }
+}
+
+export const findAndSerializeAllCrystals = async (): Promise<any> => {
+  const crystalsResults = await prisma().crystal.findMany({
+    include: { 
+        createdBy: true,
+        image: true,
+        originLocation: {select: {placeName: true}},
+        mementoLocation: {select: {placeName: true}},
+        favouritedBy: { select: { id: true }},
+      },
+  });
+
+  return crystalsResults.map(crystal => crystal && serializeCrystal(crystal))
 };
