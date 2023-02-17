8252f3e71d334018e1ca193a4e9276c7fbb12dd6
Samantha Kellow <sammii.h@icloud.com>
2023-02-17 19:49:36 +0000

    refactor pages/crystals with CrystalsGallery

--- pages/crystals/index.tsx
+++ pages/crystals/index.tsx
@@ -1,26 +1,12 @@
-import prisma from '../../lib/prisma';
 import { GetServerSideProps } from 'next';
-import { useRouter } from 'next/router'
-import { Crystal } from '@prisma/client';
-import { useCallback, useMemo, useState } from 'react';
-import { BField, BImage, BInput } from '../../components/Atoms';
-
-type ViewCrystalsProps = {
-  crystals: null | SerialisableCrystal[]
-}
-
-type SerialisableCrystal = Omit<Crystal, 'createdAt' | 'updatedAt'> & 
-{
-  id: number,
-  name: string,
-  image: number[],
-}
-& {createdAt: string, updatedAt: string}
+import { useMemo, useState } from 'react';
+import { BField, BInput } from '../../components/Atoms';
+import CrystalGallery from '../../components/Organisms/CrystalGallery';
+import { ViewCrystalsProps } from '../../lib/types/crystal';
+import { findAndSerializeAllCrystals } from '../../lib/helpers/serializeCrystalDates';
 
 const ViewCrystals: React.FC<ViewCrystalsProps> = (props) => {
-  const crystals = props.crystals;
-  const router = useRouter();
-
+  const crystals = props.crystals;  
   const [searchState, setSearchState] = useState<string>('')
 
   const searchResults = useMemo(() => {
@@ -30,13 +16,11 @@ const ViewCrystals: React.FC<ViewCrystalsProps> = (props) => {
       return (crystal.name && crystal.name.toLowerCase().includes(searchValue)) 
           || (crystal.memento && crystal.memento.toLowerCase().includes(searchValue))
           || (crystal.origin && crystal.origin.toLowerCase().includes(searchValue))
-          // || (crystal.colour && crystal.colour.map(colour => colour.includes(searchValue)))
+          || (crystal.colour && crystal.colour.map(colour => colour.toLowerCase().includes(searchValue)))
           || (crystal.chakra && crystal.chakra.includes(searchValue));
       }
     )
   }, [crystals, searchState]);
-
-  if (!crystals) return <p>Crystals not found</p>;
 
   return (
     <div className="section">
@@ -50,43 +34,16 @@ const ViewCrystals: React.FC<ViewCrystalsProps> = (props) => {
         }}
         />
       </BField>
-      
-      <div className="columns is-multiline mt-5">
-        {searchResults.map(crystal => (
-          <div className="column is-4" key={crystal.id}>
-            <a onClick={() => router.push(`/crystals/${crystal.id}`)}>
-              <BImage imageId={crystal.image[0]} />
-            </a>
-          </div>
-        ))}
-      </div>
+      <CrystalGallery crystals={searchResults} />
     </div>
   )
 }
 
 export default ViewCrystals;
 
-export const getServerSideProps: GetServerSideProps<ViewCrystalsProps> = async (context) => {
+export const getServerSideProps: GetServerSideProps<ViewCrystalsProps> = async () => {
+  let serializedCrystals = await findAndSerializeAllCrystals()
 
-  const crystalsResults = await prisma().crystal.findMany({
-    include: { 
-        createdBy: {select: {name: true}},
-        image: {select: {id: true}},
-        originLocation: {select: {placeName: true}},
-        mementoLocation: {select: {placeName: true}},
-      },
-  });
-
-  const serialisableCrystals = crystalsResults.map(crystal => crystal && {
-    ...crystal,
-    createdAt: crystal.createdAt.toISOString(),
-    updatedAt: crystal.updatedAt.toISOString(),
-    image: crystal.image.map(image => image.id),
-    createdBy: crystal.createdBy.name,
-    mementoLocation: crystal.mementoLocation?.placeName || null,
-    originLocation: crystal.originLocation?.placeName || null,
-  })
-
-  return { props: { crystals: serialisableCrystals }}
+  return { props: { crystals: serializedCrystals }}
 }
 
