5fb4df6887911fbd9217c383a8639b421bf42a03
Samantha Kellow <sammii.h@icloud.com>
2023-02-19 18:19:56 +0000

    adding head to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,23 @@
 import type { NextPage } from 'next'
+import Head from 'next/head';
 
-// const Home: NextPage<HomeProps> = (props) => {
 const Home: NextPage = () => {
   return (
-    <section className="hero is-fullheight-with-navbar">
-      <div className="hero-body">
-        <div className="container has-text-centered">
-          <h1 className="title is-size-1">Welcome to the Crystal Index 🌈</h1>
+    <div>
+      <Head>
+        <title>The Crystal Index, the most personal, beautiful and informative way to index your crystal collection.</title>
+        <link rel="icon" type="image/x-icon" href="../public/favicon.ico"></link>
+        <meta name="google" content="nositelinkssearchbox" key="sitelinks" />
+      </Head>
+      <section className="hero is-fullheight-with-navbar">
+        <div className="hero-body">
+          <div className="container has-text-centered">
+            <h1 className="title is-size-1">Welcome to the Crystal Index 🌈</h1>
+            <p>The most personal, beautiful and informative way to index your crystal collection.</p>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   )
 }
 
