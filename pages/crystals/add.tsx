2a6af78cd5d44d9dfc6ccf1a928b95d99b034abe
Samantha Kellow <sammii.h@icloud.com>
2023-03-01 21:39:55 +0000

    use CrystalForm component on `crystals/add`

--- pages/crystals/add.tsx
+++ pages/crystals/add.tsx
@@ -1,88 +1,40 @@
 import axios from 'axios';
 import { Crystal } from '@prisma/client';
-import { FormEventHandler, useCallback, useState } from 'react'
-import { BField } from '../../components/Atoms';
-import useUser from '../../lib/hooks';
+import { useCallback } from 'react'
 import type { RestrictedReactFC } from '../../lib/hooks'
-import { crystalFields, CrystalState } from '../../lib/types/crystal';
-import BImageFileUploader from '../../components/Molecules/BImageFileUploader';
+import { CrystalRequestData, CrystalProps } from '../../lib/types/crystal';
+import CrystalForm from '../../components/Organisms/CrystalForm';
+import { GetServerSideProps } from 'next';
+import prisma from '../../lib/prisma';
 
-const CreateCrystals: RestrictedReactFC<CrystalState> = () => {
-  const { userId, role } = useUser();
+const CreateCrystals: RestrictedReactFC<CrystalProps> = (props) => {
+  const { locations, crystal, form } = props
 
-  const [crystalState, setCrystalState] = useState<CrystalState>({
-    name: undefined,
-    bio: undefined,
-    otherNames: undefined,
-    chakra: [],
-    colour: [],
-    createdById: userId,
-    memento: undefined,
-    origin: undefined,
-  });
-
-  const [imageIds, setImageIds] = useState<number[]>([]);
-
-  const createCrystal: FormEventHandler = useCallback(async (event) => {
-    event.preventDefault();
-
-    // const filteredCrystalState = crystalState.filter(property => property === 'createdById')
-
+  const createCrystal = useCallback(async (crystal: CrystalRequestData) => {
     const res = await axios.post<{crystal?: Crystal, error: string}>(
       '/api/crystal/create', 
-      {crystal: crystalState, imageIds},
+      crystal,
       { headers: { 'Content-Type': 'application/json' } }
     );
-  
+    
     const result = await res.data;
     console.log("Crystal create API result", result);
-  }, [crystalState, imageIds]);
-
+  }, []);
 
   return (
-    <div className="section">
-      <div className="columns">
-        <div className="column is-5">
-          <BImageFileUploader
-            imageIds={imageIds}
-            onChange={(newImageIds: number[]) => {
-              setImageIds(newImageIds)
-            }}
-            onRemoveImage={(removedImageId: number) => {
-              setImageIds((oldImageIds) => (oldImageIds?.filter(imageId => imageId !== removedImageId)))
-            }}
-          />
-        </div>
-        <div className="column is-5">
-          <form onSubmit={createCrystal}>
-            {crystalFields.map(field => (
-              <BField label={field.label} key={field.key}>
-                <field.component 
-                  id={field.key} 
-                  placeholder={field.placeHolder} 
-                  required={field.required}
-                  options={field.options}
-                  value={crystalState[field.key]}
-                  onChange={(newValue: any) => {
-                    setCrystalState((oldCrystalState) => ({...oldCrystalState, [field.key]: newValue}))
-                  }}
-                  onClick={(newValue: any) => {
-                    setCrystalState((oldCrystalState) => ({...oldCrystalState, [field.key]: newValue}))
-                  }}
-                />
-              </BField>
-            ))}
-            {role === 'unicorn' && <button type="submit" className="button mt-4">Create</button>} 
-          </form>
-        </div>
-      </div>
-    </div>
-    
-  )
+    <CrystalForm 
+    onSubmitCrystal={createCrystal} 
+    locations={locations} 
+    crystal={crystal} 
+    form={form} />
+    )
 }
 
 CreateCrystals.requireAuth = true
 
 export default CreateCrystals
 
-
+export const getServerSideProps: GetServerSideProps<CrystalProps> = async () => {
+  const locations = await prisma().location.findMany();
+  return { props: { locations: locations, crystal: null, form: 'create'}}
+}
