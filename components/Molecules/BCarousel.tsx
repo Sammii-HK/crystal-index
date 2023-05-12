199df39e517587488321c4776d6cb09f0cfd0711
Samantha Kellow <sammii.h@icloud.com>
2023-05-12 15:46:06 +0100

    implementing carousel delete image modal prompt

--- components/Molecules/BCarousel.tsx
+++ components/Molecules/BCarousel.tsx
@@ -1,21 +1,29 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 import { BImage } from '../Atoms';
-import React from "react";
+import React, { useState } from "react";
 import { FaTimes } from "react-icons/fa";
+import classNames from "classnames";
+
+
 
 // http://react-responsive-carousel.js.org/
 {/* <Carousel showArrows={true} onChange={onChange} onClickItem={onClickItem} onClickThumb={onClickThumb}></Carousel> */}
 
 const BCarousel: React.FC<CarouselProps> = (props) => {
   const { imageIds } = props
+  const [modalState, setModalState] = useState<{state: boolean, crystalImageId?: number}>({state: false})
 
   const imageElements = imageIds?.map(id => (
     <div key={id}>
-      {props.onRemoveImage &&
+      {imageIds.length > 1 &&
         <button
-          onClick={() => props.onRemoveImage?.(id)}
-          className="button is-outlined is-pulled-right bring-to-front is-clear">
+          onClick={() => setModalState({
+            state: !modalState.state,
+            crystalImageId: id,
+          })}
+          className="button is-outlined is-pulled-right bring-to-front is-clear"
+        >
             <span className="icon is-small">
               <FaTimes />
             </span>
@@ -29,11 +37,38 @@ const BCarousel: React.FC<CarouselProps> = (props) => {
     return <>
       {imageElements}
     </>
-  else return (
-    <Carousel showArrows={false} showStatus={false} showThumbs={false}>
-      {imageElements}
-    </Carousel>
-  )
+  else return <>
+      <Carousel showArrows={false} showStatus={false} showThumbs={false}>
+        {imageElements}
+      </Carousel>
+      <div 
+        id="carousel-modal" 
+        className={classNames("modal", { "is-active": modalState.state})}
+      >
+        <div className="modal-background"></div>
+
+        <div className="modal-content">
+          <div className="box">
+            <p>Are you sure you want to delete this image?</p>
+          </div>
+        </div>
+
+        <footer className="modal-card-foot">
+          <button 
+            className="button is-danger"
+            onClick={() => props.onRemoveImage?.(modalState.crystalImageId!)}
+          >
+              Delete Image
+          </button>
+          <button 
+            className="button"
+            onClick={() => setModalState({state: !modalState.state})}
+          >
+            Cancel
+          </button>
+        </footer>
+      </div>
+    </>
 }
 
 export default BCarousel
