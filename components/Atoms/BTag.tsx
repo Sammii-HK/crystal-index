3aad42cc2ece5890f0a4009e2f0c80316d60e5e5
Samantha Kellow <sammii.h@icloud.com>
2023-06-01 21:51:15 +0100

    impriving BTags

--- components/Atoms/BTag.tsx
+++ components/Atoms/BTag.tsx
@@ -3,9 +3,15 @@ import classNames from "classnames";
 const BTag: React.FC<InputProps> = (props) => {
 	return (
 		<span
-			className={classNames("tag", "is-clickable", "is-clear", {
-				[`is-${props.label.toLowerCase()}`]: props.selected,
-			})}
+			className={
+        classNames(
+          "tag", 
+          "is-clickable", 
+          "is-clear", 
+          "has-text-weight-bold", 
+          "is-light", 
+        {[`is-${props.label.toLowerCase()}`]: props.selected}
+      )}
 			onClick={props.onToggle}
 		>
 			{props.label}
