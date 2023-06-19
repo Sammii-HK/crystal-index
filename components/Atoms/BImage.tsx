c65b1979f13a7de36b0dcfda6393e1ba5db09bd4
Samantha Kellow <sammii.h@icloud.com>
2023-06-19 14:10:04 +0100

    migrating to next image component

--- components/Atoms/BImage.tsx
+++ components/Atoms/BImage.tsx
@@ -1,7 +1,15 @@
+import Image from 'next/image'
+
 const BImage: React.FC<InputProps> = (props) => {
   return (
     <figure className="image is-square">
       <img src={'/api/image/' + props.imageId} />
+      <Image
+      src={'/api/image/' + props.imageId}
+      alt="picture of a crystal" // TODO: send alt info to component
+      width={500}
+      height={500}
+      />
     </figure>
   )
 }
