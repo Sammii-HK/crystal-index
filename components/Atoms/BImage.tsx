9c0d80074a9fa831ef05e9df20a0d64701380578
Samantha Kellow <sammii.h@icloud.com>
2023-06-19 16:16:00 +0100

    amending image componenet

--- components/Atoms/BImage.tsx
+++ components/Atoms/BImage.tsx
@@ -3,7 +3,6 @@ import Image from 'next/image'
 const BImage: React.FC<InputProps> = (props) => {
   return (
     <figure className="image is-square">
-      <img src={'/api/image/' + props.imageId} />
       <Image
       src={'/api/image/' + props.imageId}
       alt="picture of a crystal" // TODO: send alt info to component
