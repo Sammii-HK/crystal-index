dba9d6980305b1c0b3376b2c52df067859a9d697
Samantha Kellow <sammii.h@icloud.com>
2023-06-20 21:53:42 +0100

    aqmending Head metadata

--- components/common/Layout.tsx
+++ components/common/Layout.tsx
@@ -1,5 +1,14 @@
 import Head from 'next/head'
+import { Metadata } from 'next'
 import Navbar from './Navbar'
+
+export const metadata: Metadata = {
+  title: {
+    default: 'The Crystal Index',
+    template: '%s | The Crystal Index',
+  },
+  description: 'The most personal, beautiful and informative way to discover crystal meanings and index your crystal collection.'
+}
 
 // export default ({ children: React.FC }) => {
 const Layout: React.FC<any> = (props) => {
@@ -7,9 +16,9 @@ const Layout: React.FC<any> = (props) => {
     <div className="container is-fullheight">
       <Head>
         <title>The Crystal Index, the most personal, beautiful and informative way to index your crystal collection.</title>
-        <meta name="Crystal Index" content="An Index of Crystals" />
+        <meta name="The Crystal Index" content="An Index of Crystals" />
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <link rel="icon" type="image/x-icon" href="favicon.ico"></link>
+        <link rel="icon" href="/favicon.ico" sizes="any"></link>
         <meta name="google" content="nositelinkssearchbox" key="sitelinks" />
       </Head>
 
