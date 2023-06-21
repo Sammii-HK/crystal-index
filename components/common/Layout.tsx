26fa9ba672a7af0238ddbcb8c97a303964997c23
Samantha Kellow <sammii.h@icloud.com>
2023-06-21 10:17:02 +0100

    Layout refactor

--- components/common/Layout.tsx
+++ components/common/Layout.tsx
@@ -1,25 +1,16 @@
-import Head from 'next/head'
-import { Metadata } from 'next'
+import Head from 'next/head';
 import Navbar from './Navbar'
-
-export const metadata: Metadata = {
-  title: {
-    default: 'The Crystal Index',
-    template: '%s | The Crystal Index',
-  },
-  description: 'The most personal, beautiful and informative way to discover crystal meanings and index your crystal collection.'
-}
 
 // export default ({ children: React.FC }) => {
 const Layout: React.FC<any> = (props) => {
   return (
     <div className="container is-fullheight">
       <Head>
-        <title>The Crystal Index, the most personal, beautiful and informative way to index your crystal collection.</title>
+        <title>The Crystal Index</title>
         <meta name="The Crystal Index" content="An Index of Crystals" />
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <link rel="icon" href="/favicon.ico" sizes="any"></link>
         <meta name="google" content="nositelinkssearchbox" key="sitelinks" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
       </Head>
 
       <Navbar />
@@ -36,4 +27,17 @@ const Layout: React.FC<any> = (props) => {
 }
 
 export default Layout;
+
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode
+// }) {
+//   return (
+//     <html lang="en">
+//       {/* <Navbar /> */}
+//       <body>{children}</body>
+//     </html>
+//   )
+// }
 
