56436da9f12e0916b397eee3665aa5022852171c
Samantha Kellow <sammii.h@icloud.com>
2023-06-21 11:31:06 +0100

    migrating pages/index to app/page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,20 @@
 import { Metadata } from 'next'
 
 // export const metadata: Metadata = {
-//   title: {
-//     default: 'The Crystal Index',
-//     template: '%s | The Crystal Index',
-//   },
-//   description: 'The most personal, beautiful and informative way to discover crystal meanings and index your crystal collection.',
-//   // icons: {
-//   //   icon: '/favicon.ico'
-//   // },
-//   keywords: ['crystals', 'identification' ]
+//   title: 'Homepage'
 // }
 
-export default function Page() {}
+export default function Page() {
+  return (
+    <div className="main-container">
+      <section className="hero is-fullheight-with-navbar">
+        <div className="hero-body">
+          <div className="container has-text-centered">
+            <h1 className="title is-size-1">Welcome to the Crystal Index 🌈</h1>
+            <p>The most personal, beautiful and informative way to index your crystal collection.</p>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
