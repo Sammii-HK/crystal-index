52e58044a8fed077a98dd450a12a5250f4464321
Samantha Kellow <sammii.h@icloud.com>
2023-06-21 12:55:20 +0100

    adding back button to crystal view

--- pages/crystals/[id]/index.tsx
+++ pages/crystals/[id]/index.tsx
@@ -64,51 +64,56 @@ const ViewCrystal: React.FC<ViewCrystalProps> = (props) => {
   if (!crystal) return <p>Crystal not found</p>;
 
   return (
-    <div className="section container">
-      <div className='columns my-5 is-mobile is-centered'>
-        <div className="column p-0 is-5 is-offset-1 is-6-mobile is-offset-0-mobile">
-          <h1 className='title is-capitalized'>{crystal.name}</h1>
-        </div>
-        <div className="column is-flex is-justify-content-flex-end p-0 is-5 is-1-mobile">
-          <div className="columns is-mobile">
-            {(userId === crystal.createdById) && 
-              <div className="column">
-                <button type="button" className="button mb-4 is-pulled-right" onClick={editCrystal}>Edit</button>
-              </div>
-            }
+    <div className="section">
+        <a className='is-6' onClick={() => router.push('/crystals')}>
+          Back to crystals
+        </a>
+      <div className="container">
+        <div className='columns my-5 is-mobile is-centered'>
+          <div className="column p-0 is-5 is-offset-1 is-6-mobile is-offset-0-mobile">
+            <h1 className='title is-capitalized'>{crystal.name}</h1>
+          </div>
+          <div className="column is-flex is-justify-content-flex-end p-0 is-5 is-1-mobile">
+            <div className="columns is-mobile">
+              {(userId === crystal.createdById) && 
+                <div className="column">
+                  <button type="button" className="button mb-4 is-pulled-right" onClick={editCrystal}>Edit</button>
+                </div>
+              }
 
-            {userId && 
-              <div className="column">
-                <button className="button is-ghost" onClick={handleFavouriteCrystal}>
-                  <span className={classNames("icon", { 'has-text-pink': isFavourite})}>  
-                    <FaHeart />
-                  </span>
-                </button>
-              </div>
-            }
+              {userId && 
+                <div className="column">
+                  <button className="button is-ghost" onClick={handleFavouriteCrystal}>
+                    <span className={classNames("icon", { 'has-text-pink': isFavourite})}>  
+                      <FaHeart />
+                    </span>
+                  </button>
+                </div>
+              }
 
+            </div>
           </div>
         </div>
-      </div>
-      <div className="columns pt-2 is-centered is-multiline is-flex">
-        <div className="column p-0 is-10-mobile is-5 is-offset-1 is-offset-0-mobile">
-          <BCarousel imageIds={crystal.image} />
-        </div>
-        <div className="column is-4 is-offset-1 is-10-mobile">
-          {infoFieldsToShow.map(field => (
-            crystal.crystalInfo?.[field] && <div key={field}>
-              <p className='my-3'>
-                <span className='has-text-weight-bold is-capitalized'>{field} </span><br />
-                {crystal.crystalInfo[field]}
-              </p>
-            </div>
-          ))}
-          {tagsToShow.map(field => (
-            crystal.crystalInfo?.[field]?.length && <div key={field} className="mb-4">
-              <h1 className='has-text-weight-bold is-capitalized mb-3'>{field}</h1>
-              <BTags options={crystal.crystalInfo?.[field]} value={crystal.crystalInfo?.[field]} />
-            </div>
-          ))}
+        <div className="columns pt-2 is-centered is-multiline is-flex">
+          <div className="column p-0 is-10-mobile is-5 is-offset-1 is-offset-0-mobile">
+            <BCarousel imageIds={crystal.image} />
+          </div>
+          <div className="column is-4 is-offset-1 is-10-mobile">
+            {infoFieldsToShow.map(field => (
+              crystal.crystalInfo?.[field] && <div key={field}>
+                <p className='my-3'>
+                  <span className='has-text-weight-bold is-capitalized'>{field} </span><br />
+                  {crystal.crystalInfo[field]}
+                </p>
+              </div>
+            ))}
+            {tagsToShow.map(field => (
+              crystal.crystalInfo?.[field]?.length && <div key={field} className="mb-4">
+                <h1 className='has-text-weight-bold is-capitalized mb-3'>{field}</h1>
+                <BTags options={crystal.crystalInfo?.[field]} value={crystal.crystalInfo?.[field]} />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
