32b6201a626c07aee5cc221150abac1f5af293ff
Samantha Kellow <sammii.h@icloud.com>
2023-06-21 13:05:12 +0100

    layout body suppressHydrationWarning

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 ) {
   return (
     <html lang="en">
-      <body>
+      <body suppressHydrationWarning={true}>
         <NextAuthProvider>
           <Navbar />
           <>{children}</>
